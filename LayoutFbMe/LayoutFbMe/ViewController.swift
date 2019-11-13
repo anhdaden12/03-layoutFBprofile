b0dbd83f7590285380b28f82e6007fbb1c78ccde
Ngoc <ngocbaongo.qn@gmail.com>
2019-11-13 15:25:51 +0700

    so lazy

---
+++ LayoutFbMe/LayoutFbMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LayoutFbMe
+//
+//  Created by Apple on 11/13/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
